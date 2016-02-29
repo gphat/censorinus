74a562242e8af46be078987af5861d46d62fe4a5
Tom Switzer <thomas@stripe.com>
2016-02-29 12:06:23 -0500

    Move Metric out of package object.

---
+++ src/main/scala/github/gphat/censorinus/Metric.scala
@@ -0,0 +1,10 @@
+package github.gphat
+
+case class Metric(
+  name: String,
+  value: String,
+  metricType: String,
+  sampleRate: Double = 1.0,
+  tags: Seq[String] = Seq.empty,
+  unit: String = ""
+)
