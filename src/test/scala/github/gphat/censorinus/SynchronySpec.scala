acf66b8a1064d17212af6f397d23738db08df98b
Tom Switzer <thomas@stripe.com>
2016-03-03 11:21:02 -0500

    Add test for blocking queues in Client.

--- src/test/scala/github/gphat/censorinus/SynchronySpec.scala
+++ src/test/scala/github/gphat/censorinus/SynchronySpec.scala
@@ -7,16 +7,18 @@ import github.gphat.censorinus.statsd.Encoder
 class SynchronySpec extends FlatSpec with Matchers with Eventually {
 
   "Client" should "deal with gauges" in {
-    val s = new TestSender()
+    val s = new TestSender(1)
     val client = new Client(encoder = Encoder, sender = s)
 
+    // Queue up a message in the sender to ensure we can't publish yet.
+    s.buffer.offer("BOO!")
+    s.buffer.size should be (1)
+
     client.enqueue(Metric(name = "foobar", value = "1.0", metricType = "g"))
-    s.getBuffer.size should be (0) // Won't be there yet
-    eventually {
-      val m = s.getBuffer(0)
-      m should include ("foobar")
-      client.shutdown
-    }
+    s.buffer.size should be (1) // New metric won't be there yet
+    s.awaitMessage() should be ("BOO!")
+    s.awaitMessage() should include ("foobar")
+    client.shutdown
   }
 
   it should "be synchronous" in {
@@ -24,7 +26,7 @@ class SynchronySpec extends FlatSpec with Matchers with Eventually {
     val client = new Client(encoder = Encoder, sender = s, asynchronous = false)
 
     client.enqueue(Metric(name = "foobar", value = "1.0", metricType = "g"))
-    val m = s.getBuffer(0)
+    val m = s.buffer.poll()
     m should include ("foobar")
     client.shutdown
   }
